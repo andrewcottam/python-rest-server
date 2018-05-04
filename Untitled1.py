7bdc800f51ec5f1af0538456a4365648b4ba08a9
Andrew Cottam <a.cottam@gmail.com>
2018-05-04 14:56:42 +0000

    marxan - refactored to return vector tile optimised data for mapping

--- Untitled1.py
+++ Untitled1.py
@@ -1,5 +1,5 @@
 import pandas, json
-df= pandas.read_csv("/home/ubuntu/workspace/marxan/Marxan243/MarxanData_unix/andrew/Sample scenario/output/output_ssoln.txt").pivot(index='number',columns='planning_unit',values='planning_unit')
+df= pandas.read_csv("/home/ubuntu/workspace/marxan/Marxan243/MarxanData_unix/andrew/Sample scenario/output/output_r00001.txt").pivot(index='number',columns='planning_unit',values='planning_unit')
 transposed = df[df.columns].apply(lambda x: ','.join(x.dropna().astype(int).astype(str)),axis=1)
 ssoln = [[i,[int(n) for n in j.split(",")]] for (i,j) in transposed.items()]
 print json.loads(json.dumps(ssoln))
